b24d5bbdfd495eaade1bb51bfc849a42042e7f3f
anhrDuyao <anhr@duyao001.com>
2018-07-16 12:35:24 +0800

    sh

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var data = require('../data/menus.json');
 var config = require('../config');
 // var child_process = require('child_process');
-var child_process = require('child_process');
+var spawn = require('child_process').spawn
 var path = require('path');
 /* GET home page. */
 var src = path.join(__dirname, '../upload.sh');
-console.log(src,'<<<<<<<<<<<<<src')
+console.log(src, '<<<<<<<<<<<<<src')
 
 router.get('/', function (req, res, next) {
   res.render('index', Object.assign({
@@ -16,12 +16,19 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/upload', function (req, res, next) {
-  child_process.execFile(src, function (error, stdout, stderr) {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  });
+  rumCommand('sh', src, function (resp) { // 执行 autoBuild.sh 脚本文件
+    console.log(resp)
+  })
 });
 
+function rumCommand(cmd, args, callback) {
+  var child = spawn(cmd, args)
+  var response = ''
+  child.stdout.on('data', function (buffer) {
+    response += buffer.toString();
+  })
+  child.stdout.on('end', function () {
+    callback(response)
+  })
+}
 module.exports = router;

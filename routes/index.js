ff021c3620156a5036e69a350d5ead6b2ab2cd85
anhrDuyao <anhr@duyao001.com>
2018-07-18 14:19:27 +0800

    啦啦啦😋😋😋😋

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var data = require('../data/menus.json');
 var config = require('../config');
+var request = require('request');
 // var child_process = require('child_process');
 var spawn = require('child_process').spawn
 var path = require('path');
@@ -28,7 +29,37 @@ router.get('/upload', function (req, res, next) {
     errorCode:200
   });
 });
-
+router.get('/getMenu', function (req, res, next) {
+  res.send(data);
+});
+// 请求排行榜列表
+// router.get('/getRanking', function (req, res, next) {
+//   request({
+//     url: 'http://api.gps116.cn/ranking/gender?isfree=1&freeappid=1281437555',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     }
+//   }, function (error, response, body) {
+//     res.send(body)
+//   });
+// });
+// 请求排行榜列表
+router.get('/getRanking', function (req, res, next) {
+  var id = req.query.RankId || 'gender';
+  var isfree = req.query.isfree || 1;
+  var freeappid = req.query.freeappid || 1281437555;
+  var _url = `http://api.gps116.cn/ranking/${id}?isfree=${isfree}&freeappid=${freeappid}`;
+  request({
+    url: _url,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }, function (error, response, body) {
+    res.send(body)
+  });
+});
 function rumCommand(cmd, args, callback) {
   var child = spawn(cmd, args)
   var response = ''

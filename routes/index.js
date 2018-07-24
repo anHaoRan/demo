d76702c4232d0688f89a158617a61a3643c74288
anhrDuyao <anhr@duyao001.com>
2018-07-24 15:51:43 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ var request = require('request');
 // var child_process = require('child_process');
 var spawn = require('child_process').spawn
 var path = require('path');
+var _headers = {
+  
+  'Accept': 'application/json',
+  'Content Type': 'application/json; charset=utf-8'
+  
+};
 /* GET home page. */
 // 首页
 router.get('/', function (req, res, next) {
@@ -19,36 +25,47 @@ router.get('/opciton', function (req, res, next) {
     title: '系统操作页'
   }, config));
 });
+// 书籍详情页
+router.get('/detail/:id', function (req, res, next) {
+  var id = req.params.id;
+  var time = new Date().getTime();
+  var _url = `http://api.gps116.cn/book/${id}?isfree=1&freeappid=1281437555`;
+  request({
+    url: _url,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }, function (error, response, body) {
+    // var obj = JSON.parse(body);
+    var obj = JSON.stringify(body);
+    console.log(obj)
+    res.render('detail', Object.assign({
+      title: data.title + '书籍详情页'
+    }, config, {
+      data: obj
+    }));
+  });
+});
 
 router.get('/upload', function (req, res, next) {
   rumCommand('sh', ['./upload.sh'], function (resp) { // 执行 autoBuild.sh 脚本文件
     console.log(resp)
   })
   res.send({
-    data:'su',
-    errorCode:200
+    data: 'su',
+    errorCode: 200
   });
 });
 router.get('/getMenu', function (req, res, next) {
   res.send(data);
 });
 // 请求排行榜列表
-// router.get('/getRanking', function (req, res, next) {
-//   request({
-//     url: 'http://api.gps116.cn/ranking/gender?isfree=1&freeappid=1281437555',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json'
-//     }
-//   }, function (error, response, body) {
-//     res.send(body)
-//   });
-// });
-// 请求排行榜列表
 router.get('/getRanking', function (req, res, next) {
   var id = req.query.RankId || 'gender';
   var isfree = req.query.isfree || 1;
   var freeappid = req.query.freeappid || 1281437555;
+
   var _url = `http://api.gps116.cn/ranking/${id}?isfree=${isfree}&freeappid=${freeappid}`;
   request({
     url: _url,
@@ -60,6 +77,7 @@ router.get('/getRanking', function (req, res, next) {
     res.send(body)
   });
 });
+
 function rumCommand(cmd, args, callback) {
   var child = spawn(cmd, args)
   var response = ''

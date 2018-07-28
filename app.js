3064cca8ab3e373ec4e46fe873228c794b10df34
anHaoRan <810597179@qq.com>
2018-07-28 09:16:22 +0800

    1

--- app.js
+++ app.js
@@ -15,13 +15,19 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+//图片代理
+app.use('/image2', proxy({
+  target: 'http://statics.sdchjd.cn',
+  changeOrigin: true
+}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
